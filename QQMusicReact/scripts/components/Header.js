813b811f6311f1e9948e85efa954dd83d0092118
luyujing <2567549392@qq.com>
2022-05-07 16:20:25 +0800

    删除错误上传

--- QQMusicReact/scripts/components/Header.js
+++
@@ -1,9 +0,0 @@
-class Header extends React.Component {
-    render() {
-        return (React.createElement("header", null,
-            React.createElement("div", { className: "header-inner" },
-                React.createElement("img", { src: "../../imgs/logo.png", alt: "" }))));
-    }
-}
-export default Header;
-//# sourceMappingURL=Header.js.map
